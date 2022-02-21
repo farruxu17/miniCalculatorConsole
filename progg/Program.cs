d7f7cd727319ab760139ed03f95e97f72f967ca7
farruxu17 <92727105+farruxu17@users.noreply.github.com>
2022-02-21 14:23:07 +0500

    Add files via upload
    faqat + - * / operatrlari qlingan

---
+++ progg/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace proogg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1;
+            Console.WriteLine("--       Birinchi raqamni kiriting:       --");
+            num1 = Console.ReadLine();
+
+            string num2;
+            Console.WriteLine("--       Ikkinchi raqamni kiriting:       --");
+            num2 = Console.ReadLine();
+
+            double num11 = Convert.ToDouble(num1);
+            double num22 = Convert.ToDouble(num2);
+
+            string oper;
+            Console.WriteLine("--       Operatorni kiriting:  ( P/s: + - / * )       --");
+            oper = Console.ReadLine();
+
+
+            if(oper !="+" && oper != "-" && oper != "/" && oper != "*"){
+                Console.WriteLine("--       Bunday operator mavjud emas       --");
+            }
+            else if(oper == "+"){
+                double resul = num11 + num22;
+                Console.WriteLine(num11 + " + "+num22+" = "+ resul);
+            }
+            else if(oper == "-"){
+                double resul = num11 - num22;
+                Console.WriteLine(num11 + " - "+num22+" = "+ resul);
+            }
+            else if(oper == "*"){
+                double resul = num11 * num22;
+                Console.WriteLine(num11 + " * "+num22+" = "+ resul);
+            }
+            else if(oper == "/"){
+                double resul = num11 / num22;
+                Console.WriteLine(num11 + " / "+num22+" = "+ resul);
+            }
+
+            
+
+       }
+    }
+}
